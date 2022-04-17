bb7f240c1c2ab428f98063d941620cfcb432213b
秦宇航 <qinyuhangxiaoxiang@gmail.com>
2022-04-17 16:15:34 +0800

    lib: add serialize deserialize to Tree

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,8 @@
    I create to help me solve the problems
 */
 use std::cell::RefCell;
+use std::fmt::{Debug, Display, Write};
 use std::rc::Rc;
-use std::fmt::Display;
 
 /// TreeNode is leetcode provide data struct
 #[derive(Debug, PartialEq, Eq, Clone)]
@@ -74,6 +74,100 @@ pub struct Tree {
     pub root: Option<Rc<RefCell<TreeNode>>>,
 }
 
+// These codes are from quest 297
+impl Tree {
+    pub fn from_level_order_str(data: String) -> Self {
+        use std::collections::VecDeque;
+        let mut tree = Tree { root: None };
+
+        let mut chars = data
+            .split(',')
+            .into_iter()
+            .filter(|&c| c != "")
+            .collect::<VecDeque<&str>>();
+
+        if chars.len() == 0 {
+            return tree;
+        }
+
+        let root = Rc::new(RefCell::new(TreeNode::new(
+            chars.pop_front().unwrap().parse().unwrap(),
+        )));
+
+        let mut queue = std::collections::VecDeque::new();
+        queue.push_back(Rc::clone(&root));
+
+        while !queue.is_empty() {
+            let cur = queue.pop_front().unwrap();
+
+            let left_ch = chars.pop_front();
+            let right_ch = chars.pop_front();
+            if left_ch.is_some() {
+                let ch = left_ch.unwrap();
+                if ch != "null" {
+                    let node = Rc::new(RefCell::new(TreeNode::new(ch.parse().unwrap())));
+
+                    queue.push_back(Rc::clone(&node));
+                    (*cur).borrow_mut().left = Some(node);
+                };
+            }
+            if right_ch.is_some() {
+                let ch = right_ch.unwrap();
+                if ch != "null" {
+                    let node = Rc::new(RefCell::new(TreeNode::new(ch.parse().unwrap())));
+
+                    queue.push_back(Rc::clone(&node));
+                    (*cur).borrow_mut().right = Some(node);
+                }
+            }
+        }
+
+        tree.root = Some(root);
+
+        tree
+    }
+    pub fn to_level_order_str(&self) -> String {
+        let root = Some(Rc::clone(self.root.as_ref().unwrap()));
+
+        let mut res: Vec<String> = vec![];
+        let mut queue = std::collections::VecDeque::new();
+
+        queue.push_back(root);
+
+        // level travers
+        while let Some(entity) = queue.pop_front() {
+            if let Some(ref node) = entity {
+                let bo_node = Rc::clone(node);
+
+                res.push((*bo_node).borrow().val.to_string());
+
+                if (*bo_node).borrow().left.as_ref().is_some() {
+                    queue.push_back(Some(Rc::clone(&(*bo_node).borrow().left.as_ref().unwrap())));
+                } else {
+                    queue.push_back(None);
+                }
+
+                if (*bo_node).borrow().right.as_ref().is_some() {
+                    queue.push_back(Some(Rc::clone(
+                        &(*bo_node).borrow().right.as_ref().unwrap(),
+                    )));
+                } else {
+                    queue.push_back(None);
+                }
+            } else {
+                res.push(String::from("null"));
+            }
+        }
+
+        // remove the null from the end
+        while res.last() == Some(&"null".to_string()) {
+            res.pop();
+        }
+
+        res.join(",")
+    }
+}
+
 /// Linked List
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ListNode {
@@ -102,4 +196,11 @@ impl Display for ListNode {
         output.push_str("]");
         f.write_str(&output)
     }
+}
+
+#[test]
+fn test_tree_serialize_deserialize() {
+    let case = "1,2,3,4,5,6,7".to_string();
+    let t = Tree::from_level_order_str(case.clone());
+    assert_eq!(t.to_level_order_str(), case);
 }
